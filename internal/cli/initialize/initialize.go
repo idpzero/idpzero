0085085ee30d625427f3251cd2df6bebac38c324
Ross McDermott <mcdermottr@gmail.com>
2024-10-15 23:03:12 +1100

    Add CLI structure

--- internal/cli/initialize/initialize.go
+++ internal/cli/initialize/initialize.go
@@ -11,6 +11,13 @@ func Register(parent *cobra.Command) {
 		Short: "Initalize configuration for idpzero",
 		Long:  `Setup the configuration and data directory for idpzero`,
 		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// https://github.com/charmbracelet/bubbletea
+			// https://github.com/charmbracelet/lipgloss
+
+			// 1. Confirm location (cwd / .idpzero)
+			// 2. Generate folder and include default config
+
 			return nil
 		},
 	}
